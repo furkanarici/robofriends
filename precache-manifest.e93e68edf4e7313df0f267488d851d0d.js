c60a3e8b50b561c52a99200bf8412095edc78d01
Furkan Şevki ARICI <49515948+furkanarici@users.noreply.github.com>
2020-06-13 13:48:47 +0300

    Updates

---
+++ precache-manifest.e93e68edf4e7313df0f267488d851d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f406547653c680b8f10570bb6ca91bf",
+    "url": "/furkanarici/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/furkanarici/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f81b9110bd8c8afdb356",
+    "url": "/furkanarici/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/furkanarici/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/furkanarici/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81b9110bd8c8afdb356",
+    "url": "/furkanarici/robofriends/static/js/main.9b5ac43b.chunk.js"
+  },
+  {
+    "revision": "6f2bc447fdcc970d7032",
+    "url": "/furkanarici/robofriends/static/js/runtime-main.85f7bc6d.js"
+  }
+]);
